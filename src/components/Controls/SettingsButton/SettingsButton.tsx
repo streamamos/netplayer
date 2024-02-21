e641612180e16cadf6c6b4493bf87e0a0d3da76e
streamamos <134351107+streamamos@users.noreply.github.com>
2024-02-21 05:59:12 +0000

    Update SettingsButton.tsx

--- src/components/Controls/SettingsButton/SettingsButton.tsx
+++ src/components/Controls/SettingsButton/SettingsButton.tsx
@@ -7,7 +7,7 @@ import SettingsIcon from '../../icons/SettingsIcon';
 import NestedMenu from '../../NestedMenu';
 import Popover from '../../Popover';
 import ControlButton from '../ControlButton';
-import AudioMenu from './AudioMenu';
+// import AudioMenu from './AudioMenu';
 import PlaybackSpeedMenu from './PlaybackSpeedMenu';
 import QualityMenu from './QualityMenu';
 import SubtitleMenu from './SubtitleMenu';
@@ -25,7 +25,7 @@ const Menu = React.memo(() => (
     <PlaybackSpeedMenu />
     <QualityMenu />
     <SubtitleMenu />
-    <AudioMenu />
+ /*   <AudioMenu /> */
   </NestedMenu>
 ));
 
