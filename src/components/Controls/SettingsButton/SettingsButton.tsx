e4a112ad1e2684a7773becb4e23586ca237d5c30
streamamos <134351107+streamamos@users.noreply.github.com>
2024-02-21 07:20:56 +0000

    Update SettingsButton.tsx

--- src/components/Controls/SettingsButton/SettingsButton.tsx
+++ src/components/Controls/SettingsButton/SettingsButton.tsx
@@ -7,7 +7,7 @@ import SettingsIcon from '../../icons/SettingsIcon';
 import NestedMenu from '../../NestedMenu';
 import Popover from '../../Popover';
 import ControlButton from '../ControlButton';
-// import AudioMenu from './AudioMenu';
+import AudioMenu from './AudioMenu';
 import PlaybackSpeedMenu from './PlaybackSpeedMenu';
 import QualityMenu from './QualityMenu';
 import SubtitleMenu from './SubtitleMenu';
