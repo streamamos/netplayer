ee6b4318ba03b62972fa56878b5e376757856fd9
streamamos <134351107+streamamos@users.noreply.github.com>
2025-04-14 17:34:35 +0100

    lower time to ui disappear after no mouse 3

--- src/components/DefaultUI/DefaultUI.tsx
+++ src/components/DefaultUI/DefaultUI.tsx
@@ -45,7 +45,7 @@ const DefaultUI = React.forwardRef<HTMLVideoElement, NetPlayerProps>(
       }
       interactingTimeout.current = setTimeout(() => {
         setIsInteracting(false);
-      }, 50);
+      }, 1500);
     }, [setIsInteracting]);
     const uiComponents = React.useMemo(
       () => ({
